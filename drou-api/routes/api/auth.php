a10b0fa80e3b3b4dbd94d72005607600d4966b56
Ntruong2811 <nhattruong.dev3@gmail.com>
2024-04-29 20:57:51 +0700

    Update logout route in auth.php

--- drou-api/routes/api/auth.php
+++ drou-api/routes/api/auth.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\AuthController;
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate')->middleware('guest');
     Route::post('/register', [AuthController::class, 'register'])->name('auth.register')->middleware('guest');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth');
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'user'])->name('auth.user')->middleware('auth');
 });
