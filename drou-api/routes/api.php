5952fec647ff356bd28ecf19137330e831af71f5
Ntruong2811 <nhattruong.dev3@gmail.com>
2024-05-11 11:25:36 +0700

    Add app_url configuration and upload-file route

--- drou-api/routes/api.php
+++ drou-api/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
-
 
 foreach (File::allFiles(__DIR__ . '/api') as $routeFile) {
     require $routeFile->getPathname();
@@ -13,3 +13,9 @@ Route::get('/csrf-token', function () {
     return csrf_token();
 });
 
+
+Route::post('/upload-file', function (Request $request) {
+    $file = $request->file('file')->store('public');
+    $fileName = explode('/', $file)[1];
+    return config('app.app_url') . '/storage/' . $fileName;
+});
