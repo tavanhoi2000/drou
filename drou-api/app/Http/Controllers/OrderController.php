585a5e7beee5b2b8755682aa53d40610f16ed620
Ntruong2811 <nhattruong.dev3@gmail.com>
2024-05-12 18:41:11 +0700

    Add Order-related files and classes

---
+++ drou-api/app/Http/Controllers/OrderController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\OrderService;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    protected $orderService;
+
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
+    public function index(Request $request)
+    {
+        $params = $request->all();
+        $orders = $this->orderService->search($params);
+
+        return response()->json($orders);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $order = $this->orderService->create($data);
+        return response()->json($order);
+    }
+
+    public function show($id)
+    {
+        $order = $this->orderService->find($id);
+
+        return response()->json($order);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $order = $this->orderService->update($id, $data);
+
+        return response()->json($order);
+    }
+}
